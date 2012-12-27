18b1332ff2caf5f98ce44a1969b8874551547619
benjholla <benjholla@gmail.com>
2012-12-27 16:45:57 -0500

    support for media wiki installs

---
+++ plugins/media_wiki.js
@@ -0,0 +1,24 @@
+importPackage(Packages.cookie.cadger.mattslifebytes.com);
+
+var description;
+var profileImageUrl;
+var sessionUri;
+
+function processRequest(host, uri, userAgent, accept, cookies)
+{
+	description = null;
+	profileImageUrl = null;
+	sessionUri = null;
+
+	if(cookies.indexOf("mediawiki") != -1 && cookies.indexOf("session") != -1)
+	{
+		// convientently username is stored in the cookie
+		try{
+			var usernameStart = cookies.substring(cookies.indexOf("UserName=") + 9, cookies.length);
+			var username = usernameStart.substring(0, usernameStart.indexOf(";"));
+			description = "<html>MediaWiki installation on<br><font size=5>" + host + "</font><br>User: " + username;
+		} catch (err){
+			// maybe not a session
+		}
+	}
+}
