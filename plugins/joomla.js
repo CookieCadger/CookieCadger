08da45ecfdc00699509c194b68a811193b839caf
benjholla <benjholla@gmail.com>
2012-12-27 15:14:46 -0500

    Plugin support for Joomla! CMS

---
+++ plugins/joomla.js
@@ -0,0 +1,29 @@
+importPackage(Packages.cookie.cadger.mattslifebytes.com);
+
+var description;
+var profileImageUrl;
+var sessionUri;
+
+function processRequest(host, uri, userAgent, accept, cookies)
+{
+	description = null;
+	profileImageUrl = null;
+	sessionUri = null;
+
+	// deafult install of joomla has jpanesliders_ in cookie
+	// See: https://github.com/joomla/joomla-cms/blob/master/libraries/joomla/html/sliders.php
+	// See also: https://github.com/JTracker/jissues/blob/master/libraries/joomla/session/session.php
+	if(cookies.indexOf("jpanesliders_") != -1)
+	{
+		var pageContent = Packages.cookie.cadger.mattslifebytes.com.CookieCadgerInterface.readUrl("http://" + host + uri, userAgent, accept, cookies);
+		
+		if(pageContent.indexOf("Joomla!") != -1){
+			if(uri.indexOf("administrator") != -1){
+				// administrator page
+				description = "<html>Joomla! installation on<br><font size=5>" + host + "</font><br>User Role: Administrator";
+			} else {
+				description = "<html>Joomla! installation on<br><font size=5>" + host + "</font>";
+			}
+		}
+	}
+}
