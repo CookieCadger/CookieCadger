583ed781c2000bb09fd93f1f1c3ef50918195992
benjholla <benjholla@gmail.com>
2012-12-28 16:26:54 -0500

    -plugin support for GetSimple CMS -added plugin that correctly detects Wikipedia sessions, but does not seem to work with the replay attack so adding to a "disabled-plugins" folder for now

---
+++ disabled-plugins/wikipedia.js
@@ -0,0 +1,24 @@
+importPackage(Packages.cookie.cadger.mattslifebytes.com);
+
+var description;
+var profileImageUrl;
+var sessionUri;
+
+function processRequest(host, uri, userAgent, accept, cookies)
+{
+	description = null;
+	profileImageUrl = null;
+	sessionUri = null;
+
+	if(cookies.indexOf("wiki_session=") != -1)
+	{
+		// convientently username is stored in the cookie
+		try{
+			var usernameStart = cookies.substring(cookies.indexOf("wikiUserName=") + 13, cookies.length);
+			var username = usernameStart.substring(0, usernameStart.indexOf(";"));
+			description = "<html><font size=5>Wikipedia</font><br>User: " + username;
+		} catch (err){
+			// maybe not a session
+		}
+	}
+}
---
+++ plugins/getsimple.js
@@ -0,0 +1,33 @@
+importPackage(Packages.cookie.cadger.mattslifebytes.com);
+
+var description;
+var profileImageUrl;
+var sessionUri;
+
+function processRequest(host, uri, userAgent, accept, cookies)
+{
+	description = null;
+	profileImageUrl = null;
+	sessionUri = null;
+
+	if(cookies.indexOf("GS_ADMIN_USERNAME=") != -1)
+	{
+		// convientently username is stored in the cookie
+		try{
+			var usernameStart = cookies.substring(cookies.indexOf("GS_ADMIN_USERNAME=") + 18, cookies.length);
+			var username = "";
+			if(usernameStart.indexOf(";") != -1){
+				username = usernameStart.substring(0, usernameStart.indexOf(";"));
+			} else {
+				username = usernameStart.substring(0, usernameStart.length());
+			}
+			description = "<html>GetSimple CMS Installation on<br><font size=5>" + host + "</font><br>User: " + username;
+			sessionUri = "/admin";
+		} catch (err){
+			// maybe not a session
+			description = null;
+			profileImageUrl = null;
+			sessionUri = null;
+		}
+	}
+}
